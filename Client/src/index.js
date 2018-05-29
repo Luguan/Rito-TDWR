bb217cc50496b2f695eef5459a5fce1eb7eea9d2
Lukas Annsberg <lulleannsberg@gmail.com>
2018-05-30 00:15:21 +0200

    working Vue render using webpack

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,3 +1,16 @@
-import index from ./index.vue
+import App from './index.vue';
+import Vue from 'vue';
 
-alert("hejsan")
+Vue.config.productionTip = false;
+
+let appDiv = document.querySelector('#app');
+if (!appDiv) {
+  appDiv = document.createElement('div');
+  appDiv.setAttribute('id', 'app');
+  document.body.appendChild(appDiv);
+}
+
+new Vue({
+  el: appDiv,
+  render: h => h(App),
+});
