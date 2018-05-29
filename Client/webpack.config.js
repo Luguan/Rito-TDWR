57c02c761b6e550bb7dc3851a6c4a02405c866be
Lukas Annsberg <lulleannsberg@gmail.com>
2018-05-29 23:58:13 +0200

    almost working setup

--- Client/webpack.config.js
+++ Client/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -6,7 +7,17 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'index_bundle.js'
   },
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin(),
+    new VueLoaderPlugin()
   ]
 }
