06a0727663d4a32bb2421dc2936c79b478cd121d
Lukas Annsberg <lulleannsberg@gmail.com>
2018-06-03 17:23:25 +0200

    started working on returning name and region of the summoner

--- Client/src/index.vue
+++ Client/src/index.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="example">
-    {{ msg }}
     <br>
-    <input type = "text" placeholder="summoner name"></input>
-    <select>
-      <option value="EUW">EUW</option>
-      <option value="EUNE">EUNE</option>
-      <option value="BR">BR</option>
-      <option value="JP">JP</option>
-      <option value="KR">KR</option>
-      <option value="LAN">LAN</option>
-      <option value="LAS">LAS</option>
-      <option value="NA">NA</option>
-      <option value="OCE">OCE</option>
-      <option value="TR">TR</option>
-      <option value="RU">RU</option>
-      <option value="PBE">PBE</option>
-    </select>
-    <br>
+    <form @submit="submit">
+      <input type = "text" placeholder="summoner name" v-model="Username"></input>
+      <select v-model="Region">
+        <option value="EUW">EUW</option>
+        <option value="EUNE">EUNE</option>
+        <option value="BR">BR</option>
+        <option value="JP">JP</option>
+        <option value="KR">KR</option>
+        <option value="LAN">LAN</option>
+        <option value="LAS">LAS</option>
+        <option value="NA">NA</option>
+        <option value="OCE">OCE</option>
+        <option value="TR">TR</option>
+        <option value="RU">RU</option>
+        <option value="PBE">PBE</option>
+      </select>
+      <br>
+      <button>Search</button>
+    </form>
   </div>
 </template>
 
@@ -25,7 +27,13 @@
 export default {
   data () {
     return {
-      msg: 'Hello world!'
+      Username: null,
+      Region: 'EUW',
+    }
+  },
+  methods: {
+    submit:function(e) {
+      console.log(this.$router);
     }
   }
 }
